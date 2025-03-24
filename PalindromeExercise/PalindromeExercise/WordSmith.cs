af0c9aa60d71ec4c40535103f7ba59f5d1809f2d
Ankurit Dutt <ankurit145@gmail.com>
2025-03-24 13:00:48 -0700

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+           
+            return word.ToLower().SequenceEqual(word.ToLower().Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("raceCar", true)]
+        [InlineData("hello", false)]
+        [InlineData("madam", true)]
+        [InlineData("step on no pets", true)] // With spaces
+        [InlineData("world", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            // Arrange
+            var wordSmith = new WordSmith();
 
+            // Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
+
     }
 }
